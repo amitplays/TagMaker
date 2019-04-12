1c67aff5894a0af827cfe1338b7a139cc2fdf2b9
AMS-PC\Ams <streetw@yahoo.com>
2019-04-12 19:24:25 -0400

    BugsFixed

--- app/src/main/java/com/example/ams/tagmaker/Db/DataBaseHelper.java
+++ app/src/main/java/com/example/ams/tagmaker/Db/DataBaseHelper.java
@@ -2,8 +2,16 @@ package com.example.ams.tagmaker.Db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.ams.tagmaker.Models.TagListModel;
+import com.example.ams.tagmaker.Models.TagNameModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
@@ -43,24 +51,73 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     }
 
+    public boolean insertListName(int tagID, String tagName) {
+        SQLiteDatabase db2 = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("TagId", tagID);
+        contentValues.put("TagName", tagName);
+        long result = db2.insert("tagList", null, contentValues);
+
+        db2.close();
+
+        return result != -1;
+
+    }
+
     public void createTagList(String TagListTableName){
 
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL("create table if not exists "+ TagListTableName +" (TagId TEXT PRIMARY KEY NOT NULL ,TagName TEXT,TagDescription TEXT, SecureTagDescription TEXT)");
 
-
-
     }
 
 
+    public List<TagListModel> getdata(String TABLE) {
 
+        List<TagListModel> data = new ArrayList<>();
 
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from " + TABLE + " ;", null);
+        StringBuffer stringBuffer = new StringBuffer();
 
+        TagListModel dataModel;
+        while (cursor.moveToNext()) {
+            dataModel = new TagListModel();
+            String TagName = cursor.getString(cursor.getColumnIndexOrThrow("TagName"));
+            String TagDescription = cursor.getString(cursor.getColumnIndexOrThrow("TagDescription"));
+            String SecureTagDescription = cursor.getString(cursor.getColumnIndexOrThrow("SecureTagDescription"));
+            dataModel.setTagName(TagName);
+            dataModel.setTagDescription(TagDescription);
+            dataModel.setSecretDescription(SecureTagDescription);
+            stringBuffer.append(dataModel);
+            data.add(dataModel);
+        }
+        return data;
+    }
+
+    public List<TagNameModel> getListName() {
+
+        List<TagNameModel> data = new ArrayList<>();
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from tagList ;", null);
+        StringBuffer stringBuffer = new StringBuffer();
+
+        TagNameModel dataModel = null;
+        while (cursor.moveToNext()) {
+            dataModel = new TagNameModel();
+            String TagName = cursor.getString(cursor.getColumnIndexOrThrow("TagName"));
+            dataModel.setTagName(TagName);
+            stringBuffer.append(dataModel);
+            data.add(dataModel);
+        }
+        return data;
+    }
 
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-     //   db.execSQL("create table if not exists tagList (TagId TEXT PRIMARY KEY NOT NULL ,TagName TEXT,TagDescription TEXT, SecureTagDescription TEXT)");
+        db.execSQL("create table if not exists tagList (TagID INTEGER, TagName TEXT PRIMARY KEY NOT NULL )");
     }
 
     @Override
